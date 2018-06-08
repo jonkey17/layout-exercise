d6401da60e82968934561735b25c2a00410fab97
Jon Kepa Longarte Muela <jonkepa@jinnapp.com>
2018-06-08 17:20:30 +0100

    Create React App with Saas conf

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      Hello
+      </div>
+    );
+  }
+}
+
+export default App;
